a105523d9880f49572fe6d28d69e5f5b5d9b88d1
lilcryptotech <lilcryptotech@gmail.com>
2024-02-08 19:43:36 -0500

    s

---
+++ hashabit.py
@@ -0,0 +1,23 @@
+import hashlib
+
+def sha256():
+    text = input("Enter text to convert to SHA256 hash: ")
+    hash_object = hashlib.sha256(text.encode())
+    print(f'Text: {text}')
+    print(f'SHA256 Hash: {hash_object.hexdigest()}')
+
+
+    
+# end def
+
+
+
+options = input("Enter 1:")
+
+if options == "1":
+ sha256()
+
+
+
+elif options == " ":
+ print("")
