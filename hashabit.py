465885d65ccb66c02aefb8cfdbd9b1669263c4ac
lilcryptotech <lilcryptotech@gmail.com>
2024-02-10 18:36:21 -0500

    added update more hashes

--- hashabit.py
+++ hashabit.py
@@ -5,21 +5,46 @@ def sha256():
     hash_object = hashlib.sha256(text.encode())
     print(f'Text: {text}')
     print(f'SHA256 Hash: {hash_object.hexdigest()}')
+   
+
+def sha1():
+   text = input("Enter text to convert SHA1 hash")
+   hash_object = hashlib.sha1(text.encode())
+   print(f'Text: {text}')
+   print(f'SHA1 Hash: {hash_object.hexdigest}')
+   
+   
+   
+   
+def help():
+ print("welcome to hasabit Enter 1 for sha256, 2 for sha1, 3 for help")
+
+    # end def
 
 
-    
-# end def
 
 
 
-options = input("Enter 1:")
+
+options = input("Enter options: ")
 
 if options == "1":
  sha256()
 
 
 
-elif options == " ":
- print("")
- # 
+elif options == "2":
+ print("Please enter")
  
+ 
+ 
+else:
+ print("Invalid option")
+ help()
+ 
+ 
+
+
+
+
+
