7708894eeeafa3f1cc3e2514ab987293e23bfef2
lilcryptotech <lilcryptotech@gmail.com>
2024-04-03 19:41:12 -0400

    added update

--- hashabit.py
+++ hashabit.py
@@ -5,46 +5,24 @@ def sha256():
     hash_object = hashlib.sha256(text.encode())
     print(f'Text: {text}')
     print(f'SHA256 Hash: {hash_object.hexdigest()}')
-   
 
 def sha1():
-   text = input("Enter text to convert SHA1 hash")
-   hash_object = hashlib.sha1(text.encode())
-   print(f'Text: {text}')
-   print(f'SHA1 Hash: {hash_object.hexdigest}')
-   
-   
-   
-   
+    text = input("Enter text to convert to SHA1 hash: ")
+    hash_object = hashlib.sha1(text.encode())
+    print(f'Text: {text}')
+    print(f'SHA1 Hash: {hash_object.hexdigest()}')
+
 def help():
- print("welcome to hasabit Enter 1 for sha256, 2 for sha1, 3 for help")
+    print("Welcome to hasabit. Enter 1 for SHA256, 2 for SHA1, 3 for help")
 
-    # end def
-
-
-
-
-
-
-options = input("Enter options: ")
-
-if options == "1":
- sha256()
-
-
-
-elif options == "2":
- print("Please enter")
- 
- 
- 
-else:
- print("Invalid option")
- help()
- 
- 
-
-
-
-
-
+while True:
+    help()
+    option = input("ENTER OPTION: ")
+    if option == "1":
+        sha256()
+    elif option == "2":
+        sha1()
+    elif option == "3":
+        help()
+    else:
+        print("Invalid option. Please try again.")
